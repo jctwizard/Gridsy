9b5790a2235af472203be5be1f095e7c7722ddb4
Claire1605 <claire1605@gmail.com>
2024-05-15 08:15:55 +0100

    grid resizing

--- script.js
+++ script.js
@@ -5,84 +5,88 @@
 // - store frame elements in an array
 // - make iframes not individually interactable
 
-var cellWidth = 128;
-var cellHeight = 128;
+var cellWidth = 128 * 2;
+var cellHeight = 128 * 2;
 var columnCount = 7;
 var rowCount = 4;
+var cellArray = [];
+var cellsLoaded = 0;
 
 window.onload = setupGrid;
 window.onresize = resizeGrid;
 
 function onkeydown(event)
 {
-    for (let x = 0; x < columnCount; x++)
-    { 
-        for (let y = 0; y < rowCount; y++)
-        {  
-            var frameId = y * rowCount + x + 1;
-            var frame = document.getElementById(frameId.toString());
-            frame.contentWindow.document.dispatchEvent(new KeyboardEvent("keydown", event));
+    for (let y = 0; y < rowCount; y++)
+    {  
+        for (let x = 0; x < columnCount; x++)
+        { 
+            var frameId = y * columnCount + x + 1;
+            cellArray[frameId].contentWindow.document.dispatchEvent(new KeyboardEvent("keydown", event));
         }
     }
 }
 
 function onkeyup(event)
 {
-    for (let x = 0; x < columnCount; x++)
-    { 
-        for (let y = 0; y < rowCount; y++)
-        {  
-            var frameId = y * rowCount + x + 1;
-            var frame = document.getElementById(frameId.toString());
-            frame.contentWindow.document.dispatchEvent(new KeyboardEvent("keyup", event));
+    for (let y = 0; y < rowCount; y++)
+    {  
+        for (let x = 0; x < columnCount; x++)
+        { 
+            var frameId = y * columnCount + x + 1;
+            cellArray[frameId].contentWindow.document.dispatchEvent(new KeyboardEvent("keyup", event));
         }
     }
 }
 
 function setupGrid()
 {
-    for (let x = 0; x < columnCount; x++)
-    { 
-        for (let y = 0; y < rowCount; y++)
-        {  
-            var frameId = y * rowCount + x + 1;
+    for (let y = 0; y < rowCount; y++)
+    {  
+        for (let x = 0; x < columnCount; x++)
+        { 
+            var frameId = y * columnCount + x + 1;
             var cell = document.createElement("iframe");
 
             cell.id = frameId.toString();
             cell.src = "1.html";//frameId.toString() + ".html";
-            document.body.appendChild(cell);
+
+            cellArray[frameId] = cell;
+            document.body.appendChild(cellArray[frameId]);
+
+            cellArray[frameId].onload = function() 
+            { 
+                cellsLoaded += 1; 
+                
+                if (cellsLoaded >= y * x) 
+                {
+                    resizeGrid();
+                }
+            };
         }
     }  
     
     addEventListener("keydown", onkeydown);
     addEventListener("keyup", onkeyup);
-
-
-    // wait here for loading to finish. resize frame function?
-    resizeGrid();
 }
 
 function resizeGrid()
 {
-    var bodyWidth = cellWidth * columnCount;
-    document.body.style.width = bodyWidth.toString() + "px";
+    var gridWidth = cellWidth * columnCount;
+    var gridHeight = cellHeight * rowCount;
 
-    var bodyHeight = cellHeight * rowCount;
-    document.body.style.height = bodyHeight.toString() + "px";
+    for (let y = 0; y < rowCount; y++)
+    {  
+        for (let x = 0; x < columnCount; x++)
+        { 
+            var frameId = y * columnCount + x + 1;
+            var left = x * cellWidth + (window.innerWidth - gridWidth) / 2;
+            var top = y * cellHeight + (window.innerHeight - gridHeight) / 2;
 
-    for (let x = 0; x < columnCount; x++)
-    { 
-        for (let y = 0; y < rowCount; y++)
-        {  
-            var frameId = y * rowCount + x + 1;
-            var frame = document.getElementById(frameId.toString());
-            var left = x * cellWidth + (window.innerWidth - bodyWidth) / 2;
-            var top = y * cellHeight + (window.innerHeight - bodyHeight) / 2;
-
-            frame.style.width = cellWidth.toString() + "px";
-            frame.style.height = cellHeight.toString() + "px";
-            frame.style.left = left.toString() + "px";
-            frame.style.top = top.toString() + "px";
+            cellArray[frameId].style.width = cellWidth.toString() + "px";
+            cellArray[frameId].style.height = cellHeight.toString() + "px";
+            cellArray[frameId].style.left = left.toString() + "px";
+            cellArray[frameId].style.top = top.toString() + "px";
         }
     }
 }
